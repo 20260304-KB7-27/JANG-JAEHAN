a9ffa0b0818b7be8b18564831c2508dde2306720
JAEHAN-JANG <twinsjh01@gmail.com>
2026-05-28 15:33:28 +0900

    0528 인DB 04_TABLE_VIEW 05_INDEX

---
+++ 05_Database/04_TABLE_VIEW/01_ddl.sql
@@ -0,0 +1,44 @@
+-- DDL (Data Definition Language)
+-- 데이터베이스 구조를 정의하는 언어
+
+DROP DATABASE IF EXISTS tabledb; -- 삭제
+CREATE DATABASE tabledb; -- 생성
+
+use tabledb;
+
+drop table usertbl;
+create table usertbl
+(
+    userID    char(8)     not null
+        primary key,
+    name      varchar(10) not null,
+    birthYear int         not null,
+    addr      char(2)     not null,
+    mobile1   char(3)     null,
+    mobile2   char(8)     null,
+    height    smallint    null check ( height < 500 ),
+    mDate     date        default (now())
+);
+
+# DROP TABLE IF EXISTS usertbl;
+
+create table buytbl
+(
+    num       int auto_increment primary key,
+    userID    char(8)  not null,
+    prodName  char(6)  not null,
+    groupName char(4)  null,
+    price     int      not null,
+    amount    smallint not null,
+
+-- FOREIGN KEY (현재 테이블컬럼명)
+    -- REFERENCES 참조할 테이블 명 ( 참조할 컬럼명 )
+#     constraint buytbl_ibfk_1
+#         foreign key (userID) references usertbl (userID)
+);
+
+-- 테이블 구조 변경
+ALTER TABLE buytbl
+    ADD CONSTRAINT fK_usertbl_buytbl
+        foreign key(userID) -- 외래키로 설정할 컬럼
+        references usertbl(userID); -- 참조할 부모테이블(컬럼)
---
+++ 05_Database/04_TABLE_VIEW/04_view.sql
@@ -0,0 +1,22 @@
+use employees;
+
+/*
+ VIEW
+ - 실제 데이터를 저장하지 않고
+ - 하나 이상의 테이블을 조회하는 SELECT문을 가상의 테이블로 사용하는 객체
+
+ - 원본이 변경되면 반영이 되어 보인다.
+ - INSERT, UPDATE, DELETE가 가능하지만 원본 자체가 왜곡될 가능성이 높음 (안하는 걸 권장)
+
+ 장점
+ - 지주 사용하는 SELECT 쿼리를 재사용할 수 있게 해줌.
+ - 보안, 편의성, 유지보수성 향상
+ */
+
+-- 참조하고 있는 테이블이 사라지만 뷰가 깨질 수 있으므로 뷰를 먼저 삭제할 것
+create or replace view v_employees
+as
+select emp_no, concat(first_name, ' ', last_name)
+from employees;
+
+select * from v_employees where emp_no = '10002';
