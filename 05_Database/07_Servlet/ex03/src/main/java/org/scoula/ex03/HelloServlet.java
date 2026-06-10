617f8e1ab2e41163fae1b536959d08bfd5f73b7b
JAEHAN-JANG <twinsjh01@gmail.com>
2026-06-10 15:42:04 +0900

    05_Database/07_Servlet/ex03

---
+++ 05_Database/07_Servlet/ex03/src/main/java/org/scoula/ex03/HelloServlet.java
@@ -0,0 +1,31 @@
+package org.scoula.ex03;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+
+        // 필터 동작을 보기 위함.
+        System.out.println("Hello-Servlet 동작함 .....");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ 05_Database/07_Servlet/ex03/src/main/webapp/index.jsp
@@ -0,0 +1,21 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<h1><%= "Hello World!" %>
+</h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a>
+<br/>
+<a href="main.jsp">Main 이동</a>
+<br/>
+<a href="main2.jsp">Main2 이동</a>
+<br/>
+<a href="board.jsp">Board 이동</a>
+<br/>
+<a href="session_cookie.jsp">세션-쿠키</a>
+</body>
+</html>
